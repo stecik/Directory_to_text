331c4e7ee16652166964acaefe28ae234bf05f17
Marek Štec <mar.stec@seznam.cz>
2022-03-07 15:59:38 +0100

    Final changes and comments added

--- constants/constants.py
+++ constants/constants.py
@@ -1,3 +1,4 @@
+# Constants
 PROGRAM_NAME = "Directory to Text"
 PROGRAM_SHORTCUT = "dtt"
 DESCRIPTION = "Returns unformated .txt file with a list of all filenames and subdirectories including files inside them starting from given directory"
--- main.py
+++
@@ -1,7 +0,0 @@
-from dtt import DTT
-from parser import args
-
-if __name__ == "__main__":
-    dtt = DTT()
-    l = dtt.dir_to_list(args.directory, args)
-    dtt.list_to_txt(args.output_file, l)
